fb6806a71a10c352d364c0b1e5ee26a083f555aa
Mohammad Dayarneh <93088580+NutrinoDaya@users.noreply.github.com>
2024-07-27 11:35:08 +0300

    updates ~>~

--- Grammar_Error_Correction/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-sentence = "i was watched tv then my father has call me for do her work"
-# corrected_sentence = correct_grammar(sentence)
-# print("Original:", sentence)
-# print("Corrected:", corrected_sentence)
